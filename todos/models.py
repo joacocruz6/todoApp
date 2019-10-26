ba5e8549bf094d1ea9269d7184ede3dcd1778d23
Joaquin Cruz <joacocruz6@gmail.com>
2019-10-26 00:25:42 -0300

    feat(ProyectoTodo): Creado modelo de los distintos proyectos que pueden tener un usuario

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class ProyectoTodo(models.Model):
+    """
+    Modelo que representa un proyecto del usuario
+    
+    Attributes:
+        nombre {CharField} -- Nombre del proyecto
+    """
+    nombre = models.CharField(max_length=255,primary_key=True)
+
+
+
